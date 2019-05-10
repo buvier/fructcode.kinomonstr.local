9f35d3d62b0f908c674c75411656d967ecd6d06c
Elijah <buvier@mail.ru>
2019-05-10 07:38:50 +0300

    09/05/2019

--- JS/mainjs.js
+++ JS/mainjs.js
@@ -2,5 +2,11 @@ objDoc = document;
 var strTitle = objDoc.title;
 var strURL = document.URL;
 alert (strTitle + ', ' + strURL);
-var strHead = document.head;
+var strHead = document.head.textContent;
 alert (strHead);
+var cPs = document.getElementsByTagName('p');
+for (i = 0; i < cPs.length; i++) {
+  cPs[i].className = 'paragraph'
+}
+var oP1 = document.getElementById('div0').textContent;
+alert (oP1);
